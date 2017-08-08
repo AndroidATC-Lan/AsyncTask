e8b310fd56cd86d1f7020c6b767fd471d3be5a94
Diego Ragazzi Rondon <ragazzi.d@gmail.com>
2017-08-08 20:45:19 -0300

    Raw get

---
+++ app/src/main/java/br/com/ragazzid/quotes/HttpConnection.java
@@ -0,0 +1,57 @@
+package br.com.ragazzid.quotes;
+
+import android.os.AsyncTask;
+
+import org.xml.sax.InputSource;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by zero on 08/08/17.
+ */
+
+public class HttpConnection extends AsyncTask<String, Integer, String> {
+
+    private String conteudo = "";
+
+    @Override
+    protected String doInBackground(String... strings) {
+        URL url = null;
+        try {
+            url = new URL(strings[0]);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                conteudo = readStream(in);
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return conteudo;
+    }
+
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while (i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+}
